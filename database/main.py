3a14ee86d4da493048f157159a438e2cd3ae8255
bmazou <codprojmail@gmail.com>
2025-07-29 21:25:43 +0200

    Add test queries

--- database/main.py
+++ database/main.py
@@ -5,23 +5,24 @@ def main():
     engine = QueryEngine()
 
     try:
-        print("--- Finding a customer ---")
-        customer = engine.find('customer', {'c_custkey': 1})
-        print(customer)
-        print("")
+        engine.run_queries(f"Customer#000000007")
+        # print("--- Finding a customer ---")
+        # customer = engine.find('customer', {'c_custkey': 1})
+        # print(customer)
+        # print("")
         
 
-        print("--- Finding number of orders with status F ---")
-        orders = engine.find('orders', {'o_orderstatus': 'F'})
-        print(f"Found {len(orders)} orders with status 'F'")
-        print("")
+        # print("--- Finding number of orders with status F ---")
+        # orders = engine.find('orders', {'o_orderstatus': 'F'})
+        # print(f"Found {len(orders)} orders with status 'F'")
+        # print("")
         
 
-        print("--- Lineitems for a customer ---")
-        lineitems = engine.find_lineitems_for_customer('Customer#000000001')
-        print("--- Results ---")
-        for item in lineitems:
-            print(item)
+        # print("--- Lineitems for a customer ---")
+        # lineitems = engine.find_lineitems_for_customer('Customer#000000001')
+        # print("--- Results ---")
+        # for item in lineitems:
+        #     print(item)
 
     finally:
         engine.disconnect_all()
