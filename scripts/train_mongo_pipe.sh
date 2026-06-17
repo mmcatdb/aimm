bd619b00c79caf78ec1b0bdf127cb82fb8904c32
bmazou <codprojmail@gmail.com>
2026-06-17 16:38:23 +0200

    Update mongo pipe to train on art

--- scripts/train_mongo_pipe.sh
+++ scripts/train_mongo_pipe.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 
-python -m scripts.flat.mongo.create_dataset mongo/tpch-2-flat tpch-2/measured-1000-30.jsonl
-
 python -m scripts.flat.mongo.create_dataset \
-  mongo/tpch-2-flat-train \
-  tpch-2/measured-1000-5.jsonl \
-  --val-dataset mongo/tpch-2-flat-val \
-  --val-ratio 0.2 \
-  --split-seed 42 \
-  --skip-first 15
-
-python -m scripts.flat.mongo.create_dataset \
-  mongo/edbt-3-flat \
-  edbt-3/measured-1000-40.jsonl \
-  --feature-extractor-dataset tpch-2-flat-train \
+  mongo/art-1-2-flat-train \
+  art-1/measured-2000-50.jsonl \
+  art-2/measured-2000-50.jsonl \
   --refresh-queryplanner \
-  --skip-first 15
+  --skip-first 30
+
+python -m scripts.flat.mongo.create_dataset \
+  mongo/tpch-2-flat-art-fe-val \
+  tpch-2/measured-1000-50.jsonl \
+  --feature-extractor-dataset art-1-2-flat-train \
+  --refresh-queryplanner \
+  --skip-first 30
+
+python -m scripts.flat.mongo.create_dataset \
+  mongo/edbt-3-flat-art-fe-test \
+  edbt-3/measured-1000-50.jsonl \
+  --feature-extractor-dataset art-1-2-flat-train \
+  --refresh-queryplanner \
+  --skip-first 30
 
 python -m scripts.flat.mongo.train \
-  mongo/tpch-2-flat-xgb-log \
-  tpch-2-flat-train \
-  tpch-2-flat-val \
-  --model-type random_forest 
-  # --n-estimators 800 \
-  # --max-depth 6 \
-  # --learning-rate 0.03 \
-  # --sample-weight log_latency
-
+  mongo/art-1-2-flat-tail-blend \
+  art-1-2-flat-train \
+  tpch-2-flat-art-fe-val \
+  --model-type tail_blend
 
 python -m scripts.flat.mongo.test \
-  mongo/tpch-2-flat-xgb-log \
-  edbt-3-flat
+  mongo/art-1-2-flat-tail-blend \
+  edbt-3-flat-art-fe-test
