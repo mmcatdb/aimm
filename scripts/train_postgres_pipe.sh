fd1e258ef45ab3385f0d137794c85b6bc63299ab
bmazou <codprojmail@gmail.com>
2026-06-17 21:07:40 +0200

    Train postgres on art

--- scripts/train_postgres_pipe.sh
+++ scripts/train_postgres_pipe.sh
@@ -1,58 +1,35 @@
 #!/bin/bash
 set -euo pipefail
 
-python -m scripts.pipeline.validate_edbt_measurements \
-  postgres \
-  edbt-2/measured-1000-20.jsonl \
-  edbt-3/measured-1000-20.jsonl
-
-# python -m scripts.flat.postgres.create_dataset \
-#   postgres/edbt-2-3-flat-train \
-#   edbt-2/measured-1000-20.jsonl \
-#   edbt-3/measured-1000-20.jsonl \
-#   --val-dataset postgres/edbt-2-3-flat-val \
-#   --val-ratio 0.2 \
-#   --split-seed 69 \
-#   --skip-first 12
-
-# python -m scripts.flat.postgres.create_dataset \
-#   postgres/tpch-2-flat-edbt-fe-test \
-#   tpch-2/measured-1000-20.jsonl \
-#   --feature-extractor-dataset edbt-2-3-flat-train \
-#   --skip-first 12
-
-# python -m scripts.flat.postgres.train \
-#   postgres/edbt-2-3-flat-rf \
-#   edbt-2-3-flat-train \
-#   edbt-2-3-flat-val \
-#   --model-type xgboost
-
-# python -m scripts.flat.postgres.test \
-#   postgres/edbt-2-3-flat-rf \
-#   tpch-2-flat-edbt-fe-test
-
+python -m scripts.flat.postgres.create_dataset \
+  postgres/art-1-4-flat-train \
+  art-1/measured-2000-40.jsonl \
+  art-2/measured-2000-40.jsonl \
+  art-3/measured-2000-40.jsonl \
+  art-4/measured-2000-40.jsonl \
+  --skip-first 25
 
 python -m scripts.flat.postgres.create_dataset \
-  postgres/edbt-2-3-flat-train \
-  tpch-2/measured-1000-20.jsonl \
-  --val-dataset postgres/edbt-2-3-flat-val \
-  --val-ratio 0.2 \
-  --split-seed 69 \
-  --skip-first 12
+  postgres/tpch-2-flat-art-fe-val \
+  tpch-2/measured-500-40.jsonl \
+  --feature-extractor-dataset art-1-4-flat-train \
+  --skip-first 25
 
 python -m scripts.flat.postgres.create_dataset \
-  postgres/tpch-2-flat-edbt-fe-test \
-  edbt-2/measured-1000-20.jsonl \
-  edbt-3/measured-1000-20.jsonl \
-  --feature-extractor-dataset edbt-2-3-flat-train \
-  --skip-first 12
+  postgres/edbt-1-4-flat-art-fe-test \
+  edbt-1/measured-200-40.jsonl \
+  edbt-2/measured-200-40.jsonl \
+  edbt-3/measured-200-40.jsonl \
+  edbt-4/measured-200-40.jsonl \
+  --feature-extractor-dataset art-1-4-flat-train \
+  --skip-first 25
 
 python -m scripts.flat.postgres.train \
-  postgres/edbt-2-3-flat-rf \
-  edbt-2-3-flat-train \
-  edbt-2-3-flat-val \
+  postgres/art-1-4-flat-rf \
+  art-1-4-flat-train \
+  tpch-2-flat-art-fe-val \
   --model-type xgboost
 
 python -m scripts.flat.postgres.test \
-  postgres/edbt-2-3-flat-rf \
-  tpch-2-flat-edbt-fe-test
+  postgres/art-1-4-flat-rf \
+  edbt-1-4-flat-art-fe-test
--- scripts/train_postgres_pipe_nn.sh
+++ scripts/train_postgres_pipe_nn.sh
@@ -1,57 +1,34 @@
 #!/bin/bash
 set -euo pipefail
 
-python -m scripts.pipeline.validate_edbt_measurements \
-  postgres \
-  edbt-2/measured-1000-20.jsonl \
-  edbt-3/measured-1000-20.jsonl
-
-# python -m scripts.neural.create_dataset \
-#   postgres/edbt-2-3-train \
-#   edbt-2/measured-1000-20.jsonl \
-#   edbt-3/measured-1000-20.jsonl \
-#   --val-dataset postgres/edbt-2-3-val \
-#   --val-ratio 0.2 \
-#   --split-seed 69 \
-#   --skip-first 12
-
-# python -m scripts.neural.create_dataset \
-#   postgres/tpch-2-edbt-fe-test \
-#   tpch-2/measured-1000-20.jsonl \
-#   --feature-extractor-dataset edbt-2-3-train \
-#   --skip-first 12
-
-# python -m scripts.neural.train \
-#   postgres/edbt-2-3-model \
-#   edbt-2-3-train \
-#   edbt-2-3-val
-
-# python -m scripts.neural.test \
-#   postgres/edbt-2-3-model/best \
-#   tpch-2-edbt-fe-test
-
-
+python -m scripts.neural.create_dataset \
+  postgres/art-1-4-train \
+  art-1/measured-2000-40.jsonl \
+  art-2/measured-2000-40.jsonl \
+  art-3/measured-2000-40.jsonl \
+  art-4/measured-2000-40.jsonl \
+  --skip-first 25
 
 python -m scripts.neural.create_dataset \
-  postgres/edbt-2-3-train \
-  tpch-2/measured-1000-20.jsonl \
-  --val-dataset postgres/edbt-2-3-val \
-  --val-ratio 0.2 \
-  --split-seed 69 \
-  --skip-first 12
+  postgres/tpch-2-art-fe-val \
+  tpch-2/measured-500-40.jsonl \
+  --feature-extractor-dataset art-1-4-train \
+  --skip-first 25
 
 python -m scripts.neural.create_dataset \
-  postgres/tpch-2-edbt-fe-test \
-  edbt-2/measured-1000-20.jsonl \
-  edbt-3/measured-1000-20.jsonl \
-  --feature-extractor-dataset edbt-2-3-train \
-  --skip-first 12
+  postgres/edbt-1-4-art-fe-test \
+  edbt-1/measured-200-40.jsonl \
+  edbt-2/measured-200-40.jsonl \
+  edbt-3/measured-200-40.jsonl \
+  edbt-4/measured-200-40.jsonl \
+  --feature-extractor-dataset art-1-4-train \
+  --skip-first 25
 
 python -m scripts.neural.train \
-  postgres/edbt-2-3-model \
-  edbt-2-3-train \
-  edbt-2-3-val
+  postgres/art-1-4-model \
+  art-1-4-train \
+  tpch-2-art-fe-val
 
 python -m scripts.neural.test \
-  postgres/edbt-2-3-model/best \
-  tpch-2-edbt-fe-test
+  postgres/art-1-4-model/best \
+  edbt-1-4-art-fe-test
