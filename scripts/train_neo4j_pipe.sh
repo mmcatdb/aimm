94de708c254eb3be41044b6900a04e127970f184
bmazou <codprojmail@gmail.com>
2026-06-17 16:41:21 +0200

    Update neo4j pipe to train on art

--- scripts/train_neo4j_pipe.sh
+++ scripts/train_neo4j_pipe.sh
@@ -2,25 +2,35 @@
 set -euo pipefail
 
 python -m scripts.flat.neo4j.create_dataset \
-  neo4j/tpch-2-flat-train \
-  tpch-2/measured-1000-40.jsonl \
-  --val-dataset neo4j/tpch-2-flat-val \
-  --val-ratio 0.2 \
-  --skip-first 20
-
+  neo4j/art-1-2-3-flat-train \
+  art-1/measured-1000-15.jsonl \
+  art-2/measured-1000-15.jsonl \
+  art-3/measured-1000-15.jsonl \
+  --skip-first 10
 
 python -m scripts.flat.neo4j.create_dataset \
-  neo4j/edbt-3-flat \
+  neo4j/tpch-2-flat-art-fe-val \
+  tpch-2/measured-1000-40.jsonl \
+  --feature-extractor-dataset art-1-2-3-flat-train \
+  --skip-first 25
+
+python -m scripts.flat.neo4j.create_dataset \
+  neo4j/edbt-1-2-3-flat-art-fe-test \
+  edbt-1/measured-1000-40.jsonl \
+  edbt-2/measured-1000-40.jsonl \
   edbt-3/measured-1000-40.jsonl \
-  --feature-extractor-dataset tpch-2-flat-train \
-  --skip-first 20
+  --feature-extractor-dataset art-1-2-3-flat-train \
+  --skip-first 25
 
 python -m scripts.flat.neo4j.train \
-  neo4j/tpch-2-flat-rf \
-  tpch-2-flat-train \
-  tpch-2-flat-val \
-  --model-type random_forest
+  neo4j/art-1-2-3-flat-rf \
+  art-1-2-3-flat-train \
+  tpch-2-flat-art-fe-val \
+  --model-type random_forest \
+  --max-depth 8 \
+  --min-samples-leaf 3 \
+  --calibration thresholded_log_isotonic
 
 python -m scripts.flat.neo4j.test \
-  neo4j/tpch-2-flat-rf \
+  neo4j/art-1-2-3-flat-rf \
   edbt-1-2-3-flat-art-fe-test
